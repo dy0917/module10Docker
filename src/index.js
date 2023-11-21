eb5fa3709ef0ec1f1da04b98dc18fa81580367e8
Kingsley <kingsley.huang@mycare.org.nz>
2023-11-21 21:22:40 +1300

    update index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Server is up and running');
+  res.send('Server is up and running !!!!');
 });
 
 app.listen(PORT, () => {
